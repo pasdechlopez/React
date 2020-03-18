d64bc6aa957fd116121d57f804e940035dfe5d48
pasdechlopez <pasdechlopez@Daniels-MacBook-Air.local>
2020-03-18 14:46:27 +0400

    dependencies

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,20 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './CardForm.css';
 
 class CardForm extends Component {
-   handleChangeForm = event => {
-      const {onChangeForm} = this.props;
-      onChangeForm(event.target.name, event.target.value);
-   };
+  handleChangeForm = event => {
+    const { onChangeForm } = this.props;
+    onChangeForm(event.target.name, event.target.value);
+  };
 
-   render() {
-      return (
-         <div data-test="card-form" className="id-form">
-            <h1 className="title">Введите свой ID number, 10 цифр</h1>
-            <input name="cardNumber" placeholder="12345678" value={this.props.cardNumber} onChange={this.handleChangeForm} />
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div data-test="card-form" className="id-form">
+        <h1 className="title">Введите свой ID number, 10 цифр</h1>
+        <input
+          name="cardNumber"
+          placeholder="12345678, 10 digits"
+          value={this.props.cardNumber}
+          onChange={this.handleChangeForm}
+        />
+      </div>
+    );
+  }
 }
 
 export default CardForm;
