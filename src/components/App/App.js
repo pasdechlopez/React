e5ea578f52ec7aeafa7f52e7710c95d9f81f34b0
pasdechlopez <pasdechlopez@Daniels-MacBook-Air.local>
2020-03-18 15:04:48 +0400

    email validation improved

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,95 +1,123 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CardForm from 'components/CardForm/CardForm';
 import Step from 'components/Step/Step';
 import PersonalForm from 'components/PersonalInfo/PersonalInfo';
 
 class App extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    cardNumber: ''
+  };
 
-      state = {
-         step: 1,
-         firstName: '',
-         lastName: '',
-         email: '',
-         cardNumber: ''
+  //проверка адекватности ввода
+  formConditions = () => {
+    const {
+      state: { email },
+      state: { firstName },
+      state: { lastName },
+      state: { cardNumber }
+    } = this;
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    switch (this.state.step) {
+      case 1:
+        return (
+          firstName !== '' && lastName !== '' && email !== '' && reg.test(email)
+        );
+      case 2:
+        // break;
+        return cardNumber.length === 10;
+      default:
+        return false;
+    }
+  };
+
+  handleClickNextForm = () => {
+    this.setState(function(prevState) {
+      return {
+        step: prevState.step + 1
       };
-   
-   //проверка адекватности ввода
-   formConditions = () => {
-      const {
-         state: {email},
-         state: {firstName},
-         state: {lastName},
-         state: {cardNumber}
-      } = this
-      const reg = new RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
-      switch (this.state.step) {
-         case 1:
-            return (firstName !== '' && lastName !== '' && 
-            email !== '' && reg.test(email));
-         case 2:
-            // break;
-            return (cardNumber.length === 10);
-         default :
-            return false
-      }
-   };
+    });
+  };
 
-   handleClickNextForm = () => {
-      this.setState(function(prevState) {
-         return {
-           step: prevState.step + 1
-         };
-       });
-   };
- 
+  handleTabClick = tabNumber => {
+    this.setState({
+      step: tabNumber
+    });
+  };
 
-   handleTabClick = (tabNumber) => {
-      this.setState({
-         step: tabNumber
-      })
-   };
+  handleFormChange = (name, value) => {
+    this.setState({
+      [name]: value
+    });
+  };
 
-   handleFormChange = (name, value) => {
-      this.setState({
-         [name]: value
-      });
-   };
+  formRender = () => {
+    switch (this.state.step) {
+      case 1:
+        return (
+          <PersonalForm
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            email={this.state.email}
+            onChangeForm={this.handleFormChange}
+          />
+        );
+      case 2:
+        return (
+          <CardForm
+            cardNumber={this.state.cardNumber}
+            onChangeForm={this.handleFormChange}
+          />
+        );
+      case 3:
+        return <p data-test="congratulations">Данные успешно сохранены</p>;
+      default:
+        return <p>Ошибка!</p>;
+    }
+  };
 
-   formRender = () => {
-      switch (this.state.step) {
-         case 1:
-            return <PersonalForm firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} onChangeForm={this.handleFormChange} />;
-         case 2:
-            return <CardForm cardNumber={this.state.cardNumber} onChangeForm={this.handleFormChange} />;
-         case 3:
-            return <p data-test="congratulations">Данные успешно сохранены</p>;
-         default: 
-            return <p>Ошибка!</p>;
-      }
-   };
+  render() {
+    const thisStep = this.state.step;
+    console.log(this.state.step);
+    console.log(this.state.email, this.state.firstName);
+    return (
+      <div className="container">
+        <div className="tab-panel">
+          <Step
+            number={1}
+            isSelected={thisStep === 1}
+            children={'Personal info'}
+            isClickable={thisStep > 1}
+            onClick={this.handleTabClick}
+          />
+          <Step
+            number={2}
+            isSelected={thisStep === 2}
+            children={'Card info'}
+            isClickable={thisStep > 2}
+            onClick={this.handleTabClick}
+          />
+          <Step number={3} isSelected={thisStep === 3} children={'Success!'} />
+        </div>
 
-   render() {
-      const thisStep = this.state.step;
-      console.log(this.state.step)
-      console.log(this.state.email, this.state.firstName)
-      return (
-         <div className="container">
-            <div className="tab-panel">
-               <Step number={1} isSelected={thisStep === 1} children={'Personal info'} isClickable={thisStep > 1} onClick={this.handleTabClick} />
-               <Step number={2} isSelected={thisStep === 2} children={'Card info'} isClickable={thisStep > 2} onClick={this.handleTabClick} />
-               <Step number={3} isSelected={thisStep === 3} children={'Success!'} />
-            </div>
-
-            <div className="form-content">
-               {this.formRender()}
-            </div>
-            <div className="button">
-               <button className="button-next" onClick={this.handleClickNextForm} disabled={!this.formConditions()} hidden={this.state.step === 3}>Next</button>
-            </div>
-         </div>
-      );
-   }
+        <div className="form-content">{this.formRender()}</div>
+        <div className="button">
+          <button
+            className="button-next"
+            onClick={this.handleClickNextForm}
+            disabled={!this.formConditions()}
+            hidden={this.state.step === 3}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
