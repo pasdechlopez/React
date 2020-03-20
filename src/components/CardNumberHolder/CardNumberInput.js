562de4b9560d8dc6b7b179ac1d96a3346f576181
pasdechlopez <pasdechlopez>
2020-03-20 15:52:47 +0300

    redone

--- src/components/CardNumberHolder/CardNumberInput.js
+++ src/components/CardNumberHolder/CardNumberInput.js
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
 
 export default class CardNumberInput extends Component {
-  constructor(props) {
-    super(props);
-    this.inputRef = React.createRef();
-    this.state = {
-      number: props.cardNumber ? this.format(props.cardNumber) : ''
-    };
-  }
+ 
+  inputRef = React.createRef();
 
-  stateChange = cardNumber => {
-    this.setState(
-      state => ({
-        number: this.format(cardNumber)
-      }),
-      () => (this.inputRef.current.value = this.state.number)
-    );
-  };
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const { cardNumber } = nextProps;
-    this.stateChange(cardNumber);
+  handleInput = () => {
+    const {
+      inputRef: {current}
+    } = this;
+    current.value ? current.value =this.format(current.value) : false;
   }
 
   format = value => {
     if (value === null) {
       return '';
     }
-    const newValue = value.toString();
+    const newValue = value.replace(/\s/g, '').trim();
     const modified = newValue
       .replace(/[^\dA-Z]/g, '')
       .replace(/(.{4})/g, '$1 ')
@@ -36,25 +24,17 @@ export default class CardNumberInput extends Component {
   };
 
   handleSubmit = () => {
-    this.stateChange('');
-  };
-
-  normalize = value => {
-    const newValue = value.replace(/\s/g, '').trim();
-    return newValue;
+    this.props.cardNumber = "";
   };
 
   render() {
-    // console.log(this.inputRef.target.value);
-
-    const { onChange } = this.props;
     return (
       <input
         maxLength="19" //ограничение числа вводимых знаков с учетом пробелов
         placeholder="Type ur Card Number"
         ref={this.inputRef}
         onSubmit={this.handleSubmit}
-        onChange={event => onChange(this.normalize(event.target.value))}
+        onChange = {this.handleInput}
       />
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import ModalButton from "./components/ModalButton/ModalButton";
-import App from "./components/App/App";
+import App from "./components/App/";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-// ReactDOM.render(<ModalButton />, document.getElementById("modal"));
