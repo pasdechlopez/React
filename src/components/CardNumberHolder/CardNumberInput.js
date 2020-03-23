390331c31a37c22ab9c84ca043d2b1a869a56e5b
pasdechlopez <pasdechlopez>
2020-03-23 10:22:09 +0300

    input component improved

--- src/components/CardNumberHolder/CardNumberInput.js
+++ src/components/CardNumberHolder/CardNumberInput.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
 
 export default class CardNumberInput extends Component {
- 
-  inputRef = React.createRef();
 
-  handleInput = () => {
-    const {
-      inputRef: {current}
-    } = this;
-    current.value ? current.value =this.format(current.value) : false;
-  }
-
-  format = value => {
-    if (value === null) {
-      return '';
-    }
-    const newValue = value.replace(/\s/g, '').trim();
-    const modified = newValue
-      .replace(/[^\dA-Z]/g, '')
-      .replace(/(.{4})/g, '$1 ')
-      .trim();
-    return modified;
-  };
-
-  handleSubmit = () => {
-    this.props.cardNumber = "";
-  };
+  handleInput = (event) => {
+    const {format} = this;
+    const {currentTarget} = event;
+    currentTarget.value = format(currentTarget.value);
+   }
+   format = value => value ? value.replace(/[^\d]/g, '').replace(/(.{4})/g, '$1').trim() : '';
 
   render() {
     return (
       <input
         maxLength="19" //ограничение числа вводимых знаков с учетом пробелов
         placeholder="Type ur Card Number"
-        ref={this.inputRef}
-        onSubmit={this.handleSubmit}
         onChange = {this.handleInput}
       />
     );
