20008d5763764f7d54fb48d260dbf824e3cb07d0
pasdechlopez <pasdechlopez>
2020-03-25 19:15:05 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './components/AppRouter/AppRouter';
-import createStore from 'store';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import App from './components/App/App';
 
 import './index.css';
+import reducer from './store/reducers/reducer';
 
-const store = createStore();
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <AppRouter />
+      {/* <AppRouter /> */}
+      <App />
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
